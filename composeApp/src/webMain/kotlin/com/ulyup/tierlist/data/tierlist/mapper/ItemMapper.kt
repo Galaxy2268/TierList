630a85ae248035e545acea85b0c1d141f349688b
Daniels Muļukins <danielsmulukinss@gmail.com>
2026-05-22 23:01:26 +0300

    Fixes

--- composeApp/src/webMain/kotlin/com/ulyup/tierlist/data/tierlist/mapper/ItemMapper.kt
+++ composeApp/src/webMain/kotlin/com/ulyup/tierlist/data/tierlist/mapper/ItemMapper.kt
@@ -2,11 +2,12 @@ package com.ulyup.tierlist.data.tierlist.mapper
 
 import com.ulyup.tierlist.domain.tierlist.model.Item
 import com.ulyup.tierlist.dto.ItemDto
+import kotlinx.browser.window
 
 fun ItemDto.toDomain(): Item = Item(
     id = id,
     tierlistId = tierlistId,
-    imageUrl = imageUrl,
+    imageUrl = window.location.origin + imageUrl,
     tier = tier,
     position = position,
 )
